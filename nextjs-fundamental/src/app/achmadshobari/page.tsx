fb755aad60232da27b3ff6afbb400fc26fef8a96
achmadshobari <achmadshobari@gmail.com>
2025-07-07 22:01:38 +0700

    mesmbuat halaman perkenalan

--- nextjs-fundamental/src/app/achmadshobari/page.tsx
+++ nextjs-fundamental/src/app/achmadshobari/page.tsx
@@ -3,7 +3,7 @@
 export default function achmadshobari() {
   return (
    <div>
-    <h1>Assalamualaikum, nama saya Achmad Shobari</h1>
+    <h1>Assalamualaikum, nama saya Achmad Shobari, Salam kenal</h1>
    </div>
   );
 }
