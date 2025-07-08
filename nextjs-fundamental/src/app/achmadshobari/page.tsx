ff4c84ea20f795496d3a47733b1e70adbafecce0
achmadshobari <achmadshobari@gmail.com>
2025-07-08 07:19:31 +0700

    implement tailwind tapi blum muncul

--- nextjs-fundamental/src/app/achmadshobari/page.tsx
+++ nextjs-fundamental/src/app/achmadshobari/page.tsx
@@ -1,8 +1,8 @@
-
+import styles from './achmad.module.css'
 
 export default function achmadshobari() {
   return (
-   <div>
+   <div className='flex items-center justify-center h-screen bg blue-500 text-white p-4'>
     <h1>Assalamualaikum, nama saya Achmad Shobari, Salam kenal</h1>
    </div>
   );
